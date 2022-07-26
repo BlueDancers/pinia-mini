9f42e3ae21df97c1627db4fc2467f62fa59d7827
vkcyan <2467230789@qq.com>
2022-07-26 19:07:52 +0800

    准备开始最后一篇文章

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { createApp } from "vue";
-import { createPinia } from "./pinia/src";
-// import { myCreatePinia } from "./super-mini-pinia/index";
-import App from "./App.vue";
+// import { createPinia } from "./pinia/src";
+import { myCreatePinia } from "./super-mini-pinia/index";
+// import App from "./App.vue";
+import App from "./App-super-mini.vue";
 
 const app = createApp(App);
 
-app.use(createPinia());
-// app.use(myCreatePinia());
+// app.use(createPinia());
+app.use(myCreatePinia());
 
 app.mount("#app");
---
+++ src/test/one.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>{{ react.num }}</div>
+    <button @click="addNummm">增加</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { reactive, ref, toRefs } from "vue";
+
+const store: any = ref({});
+const state = () => {
+  return { num: 0 };
+};
+store.value.state = state();
+const localState = toRefs(store.value.state);
+const react = reactive(Object.assign(localState));
+
+function addNummm() {
+  react.num++;
+}
+</script>
+
+<style scoped></style>
